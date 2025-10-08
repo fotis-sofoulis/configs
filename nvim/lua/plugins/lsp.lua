025f23156e22b89d2e896749d4fdea43bc09e885
Fotis Sofoulis <fotis.sofoulis@gmail.com>
2025-10-08 09:43:13 +0200

    Changed sqlls to sqls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -88,8 +88,8 @@ return { -- LSP Configuration & Plugins
 				--
 				-- When you move your cursor, the highlights will be cleared (the second autocommand).
 				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then 
-					local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+				if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+					local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
 					vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 						buffer = event.buf,
 						group = highlight_augroup,
@@ -102,19 +102,19 @@ return { -- LSP Configuration & Plugins
 						callback = vim.lsp.buf.clear_references,
 					})
 
-					vim.api.nvim_create_autocmd('LspDetach', {
-						group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+					vim.api.nvim_create_autocmd("LspDetach", {
+						group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
 						callback = function(event2)
-						  vim.lsp.buf.clear_references()
-						  vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
 						end,
 					})
 				end
 
 				if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-					map('<leader>th', function()
-						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-					end, '[T]oggle Inlay [H]ints')
+					map("<leader>th", function()
+						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+					end, "[T]oggle Inlay [H]ints")
 				end
 			end,
 		})
@@ -164,7 +164,7 @@ return { -- LSP Configuration & Plugins
 			ruff = {},
 			gopls = {},
 			jsonls = {},
-			sqlls = {},
+			sqls = {},
 			terraformls = {},
 			yamlls = {},
 			bashls = {},
